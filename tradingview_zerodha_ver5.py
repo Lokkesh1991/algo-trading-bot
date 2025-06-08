1f8286fc45d2b6645f90f7fc84cc322b6b51e9f5
Lokkesh Kumar <clokkeshkumar@gmail.com>
2025-06-08 20:15:12 +0530

    Push all files including latest token and ver5 bot

--- tradingview_zerodha_ver5.py
+++ tradingview_zerodha_ver5.py
@@ -252,5 +252,6 @@ def webhook():
         return jsonify({"status": "❌ error", "error": str(e)})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
